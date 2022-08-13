b4aa9d18fe8e56c24cd3feda1788da7c136b5555
Jhenifer <jheniferpimentel16@gmail.com>
2022-08-12 22:04:35 -0300

    Exercicio Hello Word na linguagem favorita, fiz em java

---
+++ Exercicio.java
@@ -0,0 +1,14 @@
+
+package exercicio;
+
+/**
+ *
+ * @author Jhenifer
+ */
+public class ExercicioGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+    
+}
